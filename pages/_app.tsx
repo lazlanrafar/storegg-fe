5ea0267a37895e3dbd9979ed1c9de3c643115fb3
lazlanrafar <lazlanrafar@gmail.com>
2022-10-17 11:56:50 +0700

    Slicing SignUp Photo Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,9 @@ import "../styles/checkout.css";
 import "../styles/complete-checkout.css";
 import "../styles/sign-in.css";
 import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-photo-success.css";
+import "../styles/sign-up-success.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
