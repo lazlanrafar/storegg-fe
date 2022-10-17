6076ecc87726fc6f2d09e63d91db58f852ef720c
lazlanrafar <lazlanrafar@gmail.com>
2022-10-17 13:29:03 +0700

    Slicing Member Transaction Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,8 @@ import "../styles/sign-up.css";
 import "../styles/sign-up-photo.css";
 import "../styles/sign-up-success.css";
 import "../styles/404-not-found.css";
+import "../styles/overview.css";
+import "../styles/sidebar.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
