f2bc0d122415fc2255bd05677bc5a22f4df46f7e
lazlanrafar <lazlanrafar@gmail.com>
2022-10-18 12:04:06 +0700

    Refactor Reached Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Navbar from "../components/organisms/navbar";
 import MainBanner from "../components/organisms/main-banner";
 import TransactionStep from "../components/organisms/transaction-step";
 import FeaturedGame from "../components/organisms/featured-game";
+import Reached from "../components/organisms/reached";
 
 const Home: NextPage = () => {
   useEffect(() => {
@@ -16,51 +17,7 @@ const Home: NextPage = () => {
       <MainBanner />
       <TransactionStep />
       <FeaturedGame />
-
-      <section className="reached pt-50 pb-50">
-        <div className="container-fluid">
-          <div className="d-flex flex-lg-row flex-column align-items-center justify-content-center gap-lg-0 gap-4">
-            <div className="me-lg-35">
-              <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
-                290M+
-              </p>
-              <p className="text-lg text-lg-start text-center color-palette-2 m-0">
-                Players Top Up
-              </p>
-            </div>
-            <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-            <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-            <div className="me-lg-35 ms-lg-35">
-              <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
-                12.500
-              </p>
-              <p className="text-lg text-lg-start text-center color-palette-2 m-0">
-                Games Available
-              </p>
-            </div>
-            <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-            <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-            <div className="me-lg-35 ms-lg-35">
-              <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
-                99,9%
-              </p>
-              <p className="text-lg text-lg-start text-center color-palette-2 m-0">
-                Happy Players
-              </p>
-            </div>
-            <div className="horizontal-line mt-6 mb-6 me-lg-35 ms-lg-35 d-lg-none d-block"></div>
-            <div className="vertical-line me-lg-35 ms-lg-35 d-lg-block d-none"></div>
-            <div className="me-lg-35 ms-lg-35">
-              <p className="text-4xl text-lg-start text-center color-palette-1 fw-bold m-0">
-                4.7
-              </p>
-              <p className="text-lg text-lg-start text-center color-palette-2 m-0">
-                Rating Worldwide
-              </p>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Reached />
 
       <section className="story pt-50 pb-50">
         <div className="container-xxl container-fluid">
